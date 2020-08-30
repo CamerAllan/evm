518257ef9bc4ffafe2ddd33ad8b2fedb68e8efdf
Cameron Allan <cameronallandev@gmail.com>
2020-08-31 00:32:33 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Syntax
+
+**Add new binary version:**
+`bs tf 11 ./terraform11`
+
+**Switch to binary version**
+`bs tf 11`
---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use structopt::StructOpt;
+
+// Command line arguments
+#[derive(StructOpt, Debug)]
+struct Cli {
+    // The name of the binary, defaults to filename
+    name: String,
+    // The version of the binary
+    version: String,
+    // The path to the new binary we are adding
+    path: std::path::PathBuf,
+}
+
+fn main() {
+    let args = Cli::from_args();
+    println!("{:?}", args);
+}
