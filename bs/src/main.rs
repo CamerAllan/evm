81f3ac62859af55c55540416a829a3170f1d8332
Cameron Allan <cameronallandev@gmail.com>
2020-08-31 21:25:07 +0100

    Restructure into lib and bin

--- bs/src/main.rs
+++ bs/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use dirs;
+use std::fs;
+use std::path::Path;
+use structopt::StructOpt;
+
+// Command line arguments
+#[derive(StructOpt, Debug)]
+struct Cli {
+    // The name of the binary, defaults to filename
+    name: String,
+    // The version of the binary
+    version: String,
+    // The path to the new binary we are adding
+    path: std::path::PathBuf,
+}
+
+fn main() -> std::io::Result<()> {
+    let args = Cli::from_args();
+    println!("{:?}", args);
+    Ok(())
 }
